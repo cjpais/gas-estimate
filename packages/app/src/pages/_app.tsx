c6778f6d9710c192b9ece358da25ed7c146fb5dc
CJ Pais <cj@cjpais.com>
2022-05-13 15:17:43 -0700

    strip out unecessary packages

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -1,21 +1,9 @@
 import "tailwindcss/tailwind.css";
 
 import type { AppProps } from "next/app";
-import {
-  createClient as createGraphClient,
-  Provider as GraphProvider,
-} from "urql";
-
-export const graphClient = createGraphClient({
-  url: "https://api.thegraph.com/subgraphs/name/holic/example-nft",
-});
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <GraphProvider value={graphClient}>
-      <Component {...pageProps} />
-    </GraphProvider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
---
+++ packages/cli/README.md
@@ -0,0 +1,4 @@
+# gas-estimate
+
+Would like to add a simple command line tool or bash script
+that can calculate this info instead of having to go to the web
